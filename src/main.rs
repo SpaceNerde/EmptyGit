b1029d8a0ed16c832e9b33089128282b24e911c8
Space Nerd <florianpospiech77@gmail.com>
2024-09-04 18:24:08 +0200

    display branches

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,49 @@
 use anathema::prelude::*;
 use anathema::component::Component;
+use anathema::component::Value;
+use anathema::component::List;
+use anathema::component::State;
 
-struct EmptyUi;
+use git2::Repository;
+use git2::BranchType;
+
+#[derive(State)]
+struct EmptyUiState {
+    branches: Value<List<String>>,
+}
+
+struct EmptyUi {}
 
 impl Component for EmptyUi {
-    type State = ();
+    type State = EmptyUiState;
     type Message = ();
 }
 
+
 fn main() {
-   let mut backend = TuiBackend::builder()
-        .enable_alt_screen()
-        .enable_raw_mode()
+
+    let repo = match Repository::open(".") {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open: {}", e),
+    };
+    
+    let mut empty_ui_state = EmptyUiState {
+        branches: List::empty(),
+    };
+
+    let branches = repo.branches(Some(BranchType::Remote)).unwrap();
+
+
+    for branch in branches {
+        let (branch, _branch_type) = branch.unwrap();
+
+        if let Some(name) = branch.name().unwrap() {
+            println!("{}", name.to_string());
+            empty_ui_state.branches.push(name.to_string());
+        }
+    }
+    
+    let mut backend = TuiBackend::builder()
         .hide_cursor()
         .finish()
         .unwrap();
@@ -23,7 +55,7 @@ fn main() {
             "main",
             "templates/main.aml",
             EmptyUi{},
-            ()
+            empty_ui_state
         )
         .unwrap();
 
